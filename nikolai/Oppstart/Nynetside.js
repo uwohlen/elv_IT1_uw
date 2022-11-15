bad7aa5576e01e97f954e0d2d3a860effe7da2bb
Nikolai <114134865+nikosicko@users.noreply.github.com>
2022-11-15 15:02:25 +0100

    Add files via upload

---
+++ nikolai/Oppstart/Nynetside.js
@@ -0,0 +1,39 @@
+/* #bolge{
+    background-image: url(Bolge\ 2.0.png);
+    height: 20px;
+    background-color: turquoise;
+}
+*/
+const TOPPEN=document.getElementById("toppen")
+var b = document.body.scrollHeight;
+let disable = false;
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    var x = document.body.scrollHeight;
+    const SCROLL = setInterval(() => {
+        window.scrollTo(0,document.body.scrollHeight-x);
+        x += 5
+        if (x >= 0) {
+            clearInterval(SCROLL);
+        }
+    }, 1)
+});
+
+function underwater(e){
+    if (e.keyCode===32 && disable == false){
+        disable = true
+        var x = document.body.scrollHeight;
+        const SCROLL = setInterval(() => {
+            window.scrollTo(0,document.body.scrollHeight-x);
+            x += -5
+            if (x <= 0) {
+                clearInterval(SCROLL);
+            }
+        }, 1)
+
+    }
+}
+addEventListener("keydown", underwater)
+function pussy() {
+    URL="Quiz-datamaskin.html"
+}
