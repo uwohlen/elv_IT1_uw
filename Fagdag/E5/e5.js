f9fb6c5108e2fc1d72d4900c44eade48737e149d
EirikDMortensen <114002780+EirikDMortensen@users.noreply.github.com>
2022-11-22 10:33:34 +0100

    Add files via upload

--- Fagdag/E5/e5.js
+++ Fagdag/E5/e5.js
@@ -1,1 +1,10 @@
 let poeng = 0;
+
+let navnIT1fagdag = document.getElementById("idNavn");
+navnIT1fagdag.addEventListener("keydown", lagreNavn);
+function lagreNavn(event) {
+    
+    if (event.KeyCode === 13) {
+        localStorage.setItem("")
+    }
+}
