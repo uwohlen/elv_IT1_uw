b9354be1a1b085672c1a85c19bcd3ff72c1a4e52
oskarr1pel <114060386+oskarr1pel@users.noreply.github.com>
2022-11-22 14:45:17 +0100

    Add files via upload
    ikke ferdig!

---
+++ Fagdag/E22/script.js
@@ -0,0 +1,30 @@
+var currentPoint=0;
+
+function Start(){
+    document.getElementById("mole").style.visibility='hidden';
+    document.getElementById("mud").style.visibility="hidden";
+    document.getElementById("start").style.visibility='hidden';
+
+    for(var i=0;i<2;i++){
+        document.getElementsByClassName("points")[i].style.visibility="visible";
+    }
+    for(var i=0;i<6;i++){
+        document.getElementsByClassName("dirt")[i].style.visibility="visible";
+    }
+    setInterval(() => {
+        var random=Math.floor(Math.random()*6)+1;
+        document.getElementById("mole"+random).style.visibility="visible";
+        setTimeout(() => {
+            for(var i=0;i<6;i++){
+                document.getElementsByClassName("mole")[i].style.visibility="hidden";
+            }
+        }, 900);
+    }, 1000);
+}
+
+function clicked(){
+    document.getElementById("currentPoint").innerHTML=currentPoint++;
+
+    console.log("Clicked");
+}
+
