827f1de61b7b0743a3aa959d4de3f1efde714c04
Nikolai <114134865+nikosicko@users.noreply.github.com>
2022-11-28 09:35:57 +0100

    Add files via upload

---
+++ Fagdag/E20/Spill.js
@@ -0,0 +1,17 @@
+const poengEl=document.getElementById("idPoeng")
+function starter() {
+    console.log("hei")
+    document.getElementById("pistolDiv").style.display = "block";
+    document.getElementById("start1").style.display = "none";
+}
+
+let poeng= 0
+function kill() {
+    console.log("Hei")
+    poeng++;
+    poengEl.innerHTML = "Poeng: " + poeng;
+    if (poeng == 20) {
+        console.log("morraknuller")
+        document.getElementById("nestespill").style.display="block";
+    }
+}
