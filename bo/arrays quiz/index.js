09c1dc527c5aeedbb919aa9b9b51d634c3fe4632
bostephansen <114134586+bostephansen@users.noreply.github.com>
2023-03-14 16:37:59 +0100

    lok

---
+++ bo/arrays quiz/index.js
@@ -0,0 +1,18 @@
+var quiz = [
+  {
+    sporsmaal:
+      "Jesper og Ola skal dele 1/2 liter brus likt. Hvor mange liter blir det på hver?",
+    alternativer: ["1", "1/2", "1/3", "1/4", "1/5", "1/6"],
+    fasit: "1/4",
+  },
+  {
+    sporsmaal: "Iben var 27 år i 2011. Hvor gammel var hun i 2003? ",
+    alternativer: ["17", "37", "20", "19"],
+    fasit: "19",
+  },
+  {
+    sporsmaal: "Hva er kvadratroten av 169?",
+    alternativer: ["1", "13", "169", "338"],
+    fasit: "13",
+  },
+];
