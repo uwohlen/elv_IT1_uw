52355d47e886ac788d62ed28b95915aa5ac27286
bostephansen <bosta001@osloskolen.no>
2023-05-01 13:38:24 +0200

    eeee

--- bo/arrays quiz/index.js
+++ bo/arrays quiz/index.js
@@ -1,6 +1,3 @@
-
-
-
 function init() {
   let quizEl = document.getElementById("quiz");
 let resultatEl = document.getElementById("resultat");
@@ -9,19 +6,19 @@ let resultatEl = document.getElementById("resultat");
 var quiz = [
   {
     sporsmaal:
-      "Jesper og Ola skal dele 1/2 liter brus likt. Hvor mange liter blir det på hver?",
-    alternativer: ["1", "1/2", "1/3", "1/4", "1/5", "1/6"],
-    fasit: "1/4",
+      "Hvilket draktnummer har Harry Kane",
+    alternativer: ["7", "8", "9", "10", "11", "12"],
+    fasit: "10",
   },
   {
-    sporsmaal: "Iben var 27 år i 2011. Hvor gammel var hun i 2003? ",
-    alternativer: ["17", "37", "20", "19"],
-    fasit: "19",
+    sporsmaal: "Hvilket draktnummer har Heung-Min Son?",
+    alternativer: ["7", "8", "9", "10", "11", "12"],
+    fasit: "7",
   },
   {
-    sporsmaal: "Hva er kvadratroten av 169?",
-    alternativer: ["1", "13", "169", "338"],
-    fasit: "13",
+    sporsmaal: "Hvilket draktnummer har Richarlison?",
+    alternativer: ["7", "8", "9", "10", "11", "12"],
+    fasit: "9",
   },
 ];
 
