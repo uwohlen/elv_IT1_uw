85011d72e7bcf388764e3a8646ae89ab0cbdac2f
Martin <113992957+martiaro@users.noreply.github.com>
2023-05-11 10:21:59 +0200

    Add files via upload

---
+++ martin/Handleliste/Handleliste/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Handleliste.Models;
+using Microsoft.AspNetCore.Mvc;
+using Handleliste.Data;
+using System.Diagnostics;
+
+namespace Handleliste.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly DALHandleElement dalHandleElement = new();
+        private static int ID = 1;
+        public IActionResult Index(string inputName)
+        {
+
+            if (!string.IsNullOrEmpty(inputName))
+            {
+                HandleElement handleElement = new()
+                {
+                    Id = ID++,
+                    Name = inputName,
+                    IsChecked = false
+                };
+
+                dalHandleElement.AddElement(handleElement);
+            }
+
+            HandleElementViewModel handleElementViewModel = new HandleElementViewModel
+            {
+                HandleElementer = dalHandleElement.GetElements()
+            };
+
+            return View(handleElementViewModel);
+        }
+
+        public IActionResult Done(int id)
+        {
+            HandleElement handleElement = new()
+            {
+                Id = id,
+                IsChecked = true
+            };
+
+            dalHandleElement.UpdateElement(handleElement);
+
+            var handleElementViewModel = new HandleElementViewModel
+            {
+                HandleElementer = dalHandleElement.GetElements(),
+            };
+
+            return View("Index", handleElementViewModel);
+        }
+    }
+}
---
+++ martin/Handleliste/Handleliste/Models/Error.cs
@@ -0,0 +1,9 @@
+namespace Handleliste.Models
+{
+    public class Error
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
