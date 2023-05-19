61a47bb30a4be53c61424cfb79bb769035818f6e
bostephansen <bosta001@osloskolen.no>
2023-05-19 21:26:00 +0200

    la til kommentarer i utforskende prosjekt

--- bo/utforskende prosjekt/index.js
+++ bo/utforskende prosjekt/index.js
@@ -1,12 +1,13 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-app.js"; //henter "initializeApp" fra Firebase-nettsiden
 import {
+  //henter de kodene vi trenger fra Firebase-nettsiden
   getFirestore,
   collection,
-  addDoc,
   getDocs,
 } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-firestore.js";
 
 const firebaseConfig = {
+  //lager en variabel for hvilken spesifikk Firebase-app som er vår
   apiKey: "AIzaSyAv-mPsT7Wh5Vey9pKLFiwBaCU9H7nYmIw",
   authDomain: "kinesisk-flashcard.firebaseapp.com",
   projectId: "kinesisk-flashcard",
@@ -16,22 +17,18 @@ const firebaseConfig = {
   measurementId: "G-NL116K95LQ",
 };
 
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig); //lager en variabel som henter vår app fra Firebase
 
-const db = getFirestore(app);
+const db = getFirestore(app); //henter databasen fra appen
 
-const querySnapshot = await getDocs(collection(db, "flashcards"));
+const querySnapshot = await getDocs(collection(db, "flashcards")); //henter dokumentene fra firebase, "await" gjør slik at koden ikke gjør noe annet før dette er fullført
 
-var kinesiskeOrd = [];
+var kinesiskeOrd = []; //lager variabler som vi skal redigere senere
 var norskeOrd = [];
 var quizLength = 0;
-var tilfeldig = [];
-
-for (var i = 0; i < quizLength; i++) {
-  tilfeldig.append(i);
-}
 
 querySnapshot.forEach((doc) => {
+  //lager to arrays for de norske ordene og de kinesiske, som er hentet fra databasen
   kinesiskeOrd.push(doc.data().Kinesisk);
   norskeOrd.push(doc.data().Norsk);
   quizLength += 1;
@@ -41,19 +38,15 @@ let currentGlose = 0;
 let currentSpraak = "norsk";
 
 function byttOm() {
+  //funksjon for hva som skjer når du trykker på "bytt om" knappen.
   currentSpraak = currentSpraak == "norsk" ? "kinesisk" : "norsk";
   document.getElementById("nedre").className = "text-white text-3xl hidden";
   oppdaterKort();
   oppdaterTittel();
 }
 
-document.getElementById("byttOmKnapp").addEventListener("click", byttOm);
-document.getElementById("vis").addEventListener("click", vis);
-document.getElementById("neste").addEventListener("click", neste);
-document.getElementById("forrige").addEventListener("click", forrige);
-document.getElementById("startPaaNytt").addEventListener("click", startPaaNytt);
-
 function oppdaterKort() {
+  //funksjon for å oppdatere kortene, som passer på at kortene alltid har hvert sitt språk
   document.getElementById("nedre").innerText =
     currentSpraak == "norsk"
       ? norskeOrd[currentGlose]
@@ -65,6 +58,7 @@ function oppdaterKort() {
 }
 
 function oppdaterTittel() {
+  //passer på at tittelen over kortene viser riktig språk for glosene inni
   document.getElementById("oevretittel").innerText =
     currentSpraak == "norsk" ? "Kinesisk:" : "Norsk:";
   document.getElementById("nedretittel").innerText =
@@ -72,11 +66,13 @@ function oppdaterTittel() {
 }
 
 function vis() {
+  //gjør nedre kort synlig etter du trykker på "vis"
   document.getElementById("nedre").className += "visible";
 }
 
 function neste() {
-  if (currentGlose == quizLength-1) return;
+  //går videre til neste kort i array'en, og passer på at du ikke kan trykke på neste når du har gått gjennom alle kortene
+  if (currentGlose == quizLength - 1) return;
   currentGlose += 1;
   document.getElementById("nedre").className = "text-white text-3xl hidden";
   oppdaterKort();
@@ -84,6 +80,7 @@ function neste() {
 }
 
 function forrige() {
+  //går tilbake til forrige kort, og passer på at du ikke kan gå lenger tilbake enn første kort
   if (currentGlose == 0) return;
   currentGlose -= 1;
   document.getElementById("nedre").className = "text-white text-3xl hidden";
@@ -92,14 +89,15 @@ function forrige() {
 }
 
 function startPaaNytt() {
-  currentGlose = 0
+  //stokker om rekkefølgen på kortene og starter quizzen på nytt
+  currentGlose = 0;
   document.getElementById("nedre").className = "text-white text-3xl hidden";
   [kinesiskeOrd, norskeOrd] = stokkArrays(kinesiskeOrd, norskeOrd);
   oppdaterKort();
-
 }
 
 function stokkArrays(array1, array2) {
+  //funksjon som stokker to arrays parallellt (hentet fra nettet)
   for (let i = array1.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array1[i], array1[j]] = [array1[j], array1[i]];
@@ -109,7 +107,13 @@ function stokkArrays(array1, array2) {
   return [array1, array2];
 }
 
-[kinesiskeOrd, norskeOrd] = stokkArrays(kinesiskeOrd, norskeOrd);
+document.getElementById("byttOmKnapp").addEventListener("click", byttOm); //lager event listeners for alle knappene på nettsiden, og kobler de opp mot riktig funksjon dersom de blir klikket
+document.getElementById("vis").addEventListener("click", vis);
+document.getElementById("neste").addEventListener("click", neste);
+document.getElementById("forrige").addEventListener("click", forrige);
+document.getElementById("startPaaNytt").addEventListener("click", startPaaNytt);
+
+[kinesiskeOrd, norskeOrd] = stokkArrays(kinesiskeOrd, norskeOrd); //gjør første stokken når nettsiden lastes opp for første gang
 oppdaterKort();
 oppdaterTittel();
 
--- bo/utforskende prosjekt/ny.js
+++ bo/utforskende prosjekt/ny.js
@@ -1,5 +1,6 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-app.js"; //henter "initializeApp" fra Firebase-nettsiden
 import {
+  //henter de kodene vi trenger fra Firebase-nettsiden
   getFirestore,
   collection,
   addDoc,
@@ -7,6 +8,7 @@ import {
 } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-firestore.js";
 
 const firebaseConfig = {
+  //lager en variabel for hvilken spesifikk Firebase-app som er vår
   apiKey: "AIzaSyAv-mPsT7Wh5Vey9pKLFiwBaCU9H7nYmIw",
   authDomain: "kinesisk-flashcard.firebaseapp.com",
   projectId: "kinesisk-flashcard",
@@ -16,30 +18,31 @@ const firebaseConfig = {
   measurementId: "G-NL116K95LQ",
 };
 
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig); //lager en variabel som henter vår app fra Firebase
 
-const db = getFirestore(app);
+const db = getFirestore(app); //henter databasen fra appen
 
-const querySnapshot = await getDocs(collection(db, "flashcards"));
+const querySnapshot = await getDocs(collection(db, "flashcards")); //henter dokumentene fra firebase, "await" gjør slik at koden ikke gjør noe annet før dette er fullført
 querySnapshot.forEach((doc) => {
+  //legger til hver doc til den tomme tabellen vi lagde i html-dokumentet, gjentar prosessen for hver doc
   console.log(doc.id, " => ", doc.data());
-  var element = document.createElement("tr");
+  var element = document.createElement("tr"); //lager et element av typen "tr" som er en del av en tabell
   element.innerHTML = `
   <td class="border-4 border-white text-3xl text-white">${doc.data().Norsk}</td>
   <td class="border-4 border-white text-3xl text-white">${
-    doc.data().Kinesisk
+    doc.data().Kinesisk //gir elementet html koden for å fungere som et element av en tabell
   }</td>
   `;
-  document.getElementById("table").appendChild(element);
+  document.getElementById("table").appendChild(element); //legger til elementet vi nettopp lagde i den tomme tabellen på html-siden
 });
 
-document.getElementById("nyttOrd").addEventListener("submit", nyttOrd);
-
-async function nyttOrd(e) {
+async function nyttOrd(e) {//legger til ordene i form'en til firebase databasen
   e.preventDefault();
   const docRef = await addDoc(collection(db, "flashcards"), {
     Norsk: document.getElementById("nyNorsk").value,
     Kinesisk: document.getElementById("nyKinesisk").value,
   });
-  window.location.reload();
+  window.location.reload();//reloader nettsiden slik at de nye ordene dukker opp med en gang
 }
+
+document.getElementById("nyttOrd").addEventListener("submit", nyttOrd); //utfører nyttord-funksjonen når du trykker "submit"
