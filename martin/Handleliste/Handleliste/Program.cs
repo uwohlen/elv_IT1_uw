7ca0e42d27dbb0fc5cb6e429f0919ca96880d13d
Martin <113992957+martiaro@users.noreply.github.com>
2023-05-25 22:42:22 +0200

    Add files via upload

--- martin/Handleliste/Handleliste/Program.cs
+++ martin/Handleliste/Handleliste/Program.cs
@@ -1,11 +1,13 @@
+//Konfigurer webserveren
+//Webserveren er også ansvarlig for å håndtere alt rundt nettsiden
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 WebApplication app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
@@ -16,6 +18,8 @@ app.UseStatusCodePagesWithRedirects("/Error");
 
 app.UseStaticFiles();
 
+//Ruting kobler forespørsler fra brukeren til rettkontroller og metode
+//Det kan brukes til å blant annet endre sti når nettsiden lastes
 app.UseRouting();
 
 
